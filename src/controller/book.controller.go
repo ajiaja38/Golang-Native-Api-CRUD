7c5e8d451529289389b88cef1b76c82d0d9d1264
M. Aji Perdana <ajicooljazz38@gmail.com>
2024-07-08 23:55:32 +0700

    feat: update (add, update & delete book) handler 🗺 🦚

--- src/controller/book.controller.go
+++ src/controller/book.controller.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"encoding/json"
 	"fmt"
+	"go-native-crud/src/model"
 	"go-native-crud/src/service"
 	"net/http"
 	"strconv"
@@ -29,4 +30,58 @@ func GetBookByIDHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode(book)
+}
+
+func AddBookHandler(w http.ResponseWriter, r *http.Request) {
+	var book model.Book
+	err := json.NewDecoder(r.Body).Decode(&book)
+
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to decode request body: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	service.AddBook(book)
+	w.WriteHeader(http.StatusCreated)
+}
+
+func UpdateBookHandler(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Query().Get("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid book ID", http.StatusBadRequest)
+		return
+	}
+
+	var book model.Book
+	err = json.NewDecoder(r.Body).Decode(&book)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to decode request body: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	err = service.UpdateBook(id, book)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to update book: %v", err), http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func DeleteBookHandler(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Query().Get("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid book ID", http.StatusBadRequest)
+		return
+	}
+
+	err = service.DeleteBook(id)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to delete book: %v", err), http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
